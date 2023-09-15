ceed65a4eff714550993c3d2f17e3b1e8fba7f30
Fzin <fabricio.silvaramos2001@gmail.com>
2023-09-15 00:45:38 -0300

    consegui, até aqui ta funcionando a conexão

--- python_etl/etl.py
+++ python_etl/etl.py
@@ -1,6 +1,7 @@
 import pymongo
 import pandas as pd
 import psycopg2
+from sqlalchemy import create_engine
 
 # Estabeleça a conexão com o MongoDB
 client = pymongo.MongoClient("mongodb://root:puquinha1@mongodb")  # Substitua pela URL do seu MongoDB
@@ -21,7 +22,28 @@ def conecta_db_post(): # cria uma função para conectar ao postgre
         print(f"Erro na conexão: {str(e)}")
         return None
 
-con = conecta_db_post() # executa a função
+con = conecta_db_post() 
 
 # Até aqui ta funcionando 
+
+if con:
+    try:
+        # Crie um mecanismo SQLAlchemy para usar o Pandas dentro do postgre
+        engine = create_engine('postgresql://fzin:puquinha@postgres-etl:5432/mydb')
+        
+        # Substitua 'sua_tabela' pelo nome da tabela PostgreSQL em que deseja inserir os dados
+        table_name = 'mydb'
+        
+        # Use o método to_sql do Pandas para inserir os dados no PostgreSQL
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
+        print(f'Dados inseridos em {table_name}')
+    except Exception as e:
+        print(f"Erro ao inserir dados no PostgreSQL: {str(e)}")
+    finally:
+        con.close()
+# Neste código, estamos usando o SQLAlchemy para criar um mecanismo de banco de dados e, em seguida, usando o método to_sql do Pandas para inserir o DataFrame no PostgreSQL. Certifique-se de substituir 'sua_tabela' pelo nome da tabela PostgreSQL em que você deseja inserir os dados. Além disso, ajuste as informações de conexão com o PostgreSQL conforme necessário. Certifique-se de que o SQLAlchemy esteja instalado em seu ambiente (você pode instalá-lo com pip install sqlalchemy).
+
+
+
+
 
